1f980b6d0115aad64e7822884151f14292d413a2
Konrad Kokosa <kkokosa@gritstep.com>
2017-10-25 09:36:15 +0000

    Versioning fixes.

--- TravelAgency/Properties/AssemblyInfo.cs
+++ TravelAgency/Properties/AssemblyInfo.cs
@@ -29,6 +29,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.1.*")]
